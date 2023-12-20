4cf076da84c3fa67bd2957daeeca77e32bad4082
jored <ec23455@qmul.ac.uk>
2023-12-20 21:32:02 +0000

    Adding: - policy evaluation - policy improvement - policy iteration - value iteration

--- frozenLake.py
+++ frozenLake.py
@@ -261,3 +261,204 @@ def play(env):
 
         env.render()
         print('Reward: {0}.'.format(r))
+
+def policy_evaluation(env, policy, gamma, theta, max_iterations):
+    value = np.zeros(env.n_states, dtype=np.float32)
+
+    # Loop the number of iterations or while our error is greater than
+    # the tolerance parameter
+    for i in range(max_iterations):
+        delta = 0
+        # Go through all tha states
+        for state in range(env.n_states):
+            # Store value of the state previous to any modification
+            v = value[state]
+            policy_state_product = 0
+
+            # We have just one action per state in a deterministic policy, is not necessary
+            # to loop through all the actions. Leaving this 'for' just to match the policy
+            # evaluation formula
+            # Go through all the actions in the policy
+            for a in range(env.n_actions):
+                policy_p = 0
+                # If the action is not part of the policy don't calculate anything
+                if a == policy[state]:
+                    policy_p = 1
+                else:
+                    continue
+
+                state_product = 0
+
+                # Go through all the possible next states
+                for state_prime in range(env.n_states):
+                    # Get probability of transitioning from state to state_prime
+                    # given action
+                    pt = env.p(state_prime, state, a)
+                    # Get reward of transitioning from state to state_prime given
+                    # action
+                    rt = env.r(state_prime, state, a)
+
+                    state_product += pt * (rt + (gamma * value[state_prime]))
+
+                # Store product from all possible action in policy
+                policy_state_product += state_product * policy_p
+
+            # Update value of this state
+            value[state] = policy_state_product
+
+            # Calculate the difference between our previous value and the new one
+            delta = max(delta, abs(v - value[state]))
+
+        # If our delta is smaller than the error value theta, stop
+        if delta < theta:
+            break
+
+    return value
+
+
+def policy_improvement(env, value, gamma):
+    improved_policy = np.zeros(env.n_states, dtype=int)
+
+    # Go through all the states
+    for state in range(env.n_states):
+        q = list()
+
+        # Go through all the actions
+        for a in range(env.n_actions):
+            state_prime_q = 0
+
+            # Go through all next states from the actual state
+            for state_prime in range(env.n_states):
+                # Get probability of transitioning from state to state_prime
+                # given action
+                pt = env.p(state_prime, state, a)
+                # Get reward of transitioning from state to state_prime given
+                # action
+                rt = env.r(state_prime, state, a)
+
+                state_prime_q += pt * (rt + (gamma * value[state_prime]))
+
+            # Store value of all actions of possible next states in a list
+            q.append(state_prime_q)
+
+        q = np.array(q)
+
+        # Get the best action in the current state
+        improved_policy[state] = np.argmax(q)
+
+    # Return improved policy
+    return improved_policy
+
+
+def policy_iteration(env, gamma, theta, max_iterations, policy=None):
+    if policy is None:
+        policy = np.zeros(env.n_states, dtype=int)
+    else:
+        policy = np.array(policy, dtype=int)
+
+    # Iterate the maximum number of iterations or while there are still
+    # improvements in the policy
+    for i in range(max_iterations):
+        prv_policy = policy
+        # print("Iteration ", i)
+        # Get value of current policy
+        value = policy_evaluation(env, policy, gamma, theta, max_iterations)
+        # Improve policy
+        policy = policy_improvement(env, value, gamma)
+
+        # If policy cannot improve more we have the optimal policy
+        equal = prv_policy == policy
+        if not np.any(equal == False):
+            break
+
+    # Return the optimal policy and value of tha policy
+    return policy, value
+
+
+def value_iteration(env, gamma, theta, max_iterations, value=None):
+    if value is None:
+        value = np.zeros(env.n_states)
+    else:
+        value = np.array(value, dtype=np.float32)
+
+    # Keep iterating while we have budget, or if the delta error is reached
+    for i in range(max_iterations):
+        # print("Iteration ", i)
+        delta = 0
+
+        # Go through all the states
+        for state in range(env.n_states):
+            v = value[state]
+            policy_state_product = list()
+
+            # Go through all the actions
+            for a in range(env.n_actions):
+                state_product = 0
+
+                # Go through all the next states given the current state
+                for state_prime in range(env.n_states):
+                    pt = env.p(state_prime, state, a)
+                    rt = env.r(state_prime, state, a)
+
+                    state_product += pt * (rt + (gamma * value[state_prime]))
+
+                # Store evaluation of states in a list, we will decide which is
+                # the best outside this loop
+                policy_state_product.append(state_product)
+
+            # Update value whit best evaluation
+            policy_state_product = np.array(policy_state_product)
+            value[state] = np.max(policy_state_product)
+
+            # Update delta
+            delta = max(delta, abs(v - value[state]))
+
+        # If our delta is smaller than the error value theta, stop
+        if delta < theta:
+            break
+
+    # Update policy given the optimal value
+    policy = policy_improvement(env, value, gamma)
+
+    # Return optimal policy and value
+    return policy, value
+
+
+def main():
+    seed = 0
+
+    # Big lake
+    # lake = [['&', '.', '.', '.', '.', '.', '.', '.'],
+    #         ['.', '.', '.', '.', '.', '.', '.', '.'],
+    #         ['.', '.', '.', '#', '.', '.', '.', '.'],
+    #         ['.', '.', '.', '.', '.', '#', '.', '.'],
+    #         ['.', '.', '.', '#', '.', '.', '.', '.'],
+    #         ['.', '#', '#', '.', '.', '.', '#', '.'],
+    #         ['.', '#', '.', '.', '#', '.', '#', '.'],
+    #         ['.', '.', '.', '#', '.', '.', '.', '$']]
+
+    # Small lake
+    lake = [['&', '.', '.', '.'],
+            ['.', '#', '.', '#'],
+            ['.', '.', '.', '#'],
+            ['#', '.', '.', '$']]
+
+    env = FrozenLake(lake, slip=0.1, max_steps=16, seed=seed)
+    gamma = 0.9
+
+    print('# Model-based algorithms')
+
+    print('')
+
+    print('## Policy iteration')
+    policy, value = policy_iteration(env, gamma, theta=0.001, max_iterations=128)
+    env.render(policy, value)
+
+    print('')
+
+    print('## Value iteration')
+    policy, value = value_iteration(env, gamma, theta=0.001, max_iterations=128)
+    env.render(policy, value)
+
+
+main()
